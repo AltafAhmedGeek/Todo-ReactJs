fd2dae9017f6eaa82ac776db447e071dcfea7083
altaf <altafgeek@gmail.com>
2023-10-06 23:36:33 +0530

    changed routing via useRoutes Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import Todo from './Todoo'
-import Posts from './Posts'
-import NavBar from './NavBar'
-import NotFound from './NotFound'
+import React from "react";
+import { Route, Routes, useRoutes } from "react-router";
+import Todo from "./Todoo";
+import Posts from "./Posts";
+import NavBar from "./NavBar";
+import NotFound from "./NotFound";
 
 const App = () => {
-    return (
-      <>
-        <NavBar />
-        <Routes>
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Todo />,
+    },
+    {
+      path: "posts",
+      element: <Posts />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return (
+    <>
+      <NavBar />
+      {routes}
+      {/* <Routes>
           <Route path="/" element={<Todo />} />
           <Route path="/posts/" element={<Posts />}>
             <Route path="/posts/:id" element={<Posts />} />
           </Route>
           <Route path='*' element={<NotFound/>} />
-        </Routes>
-      </>
-    );
-}
+        </Routes> */}
+    </>
+  );
+};
 
-export default App
+export default App;
