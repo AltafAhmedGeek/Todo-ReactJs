016c579efd546021712b8d2e9943ade2e21fa2d8
Altaf Ahmed <158058046+altafahmedfyntune@users.noreply.github.com>
2024-08-05 14:46:25 +0530

    fix : infinite rendering

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -4,12 +4,10 @@ import React, { useEffect, useState } from "react";
 const Posts = () => {
   const [Data, setData] = useState([]);
   useEffect(() => {
-    console.log(123123123);
-    
     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
       setData(res.data);
     });
-  });
+  },[]);
   return (
     <>
       {Data.map((data) => {
